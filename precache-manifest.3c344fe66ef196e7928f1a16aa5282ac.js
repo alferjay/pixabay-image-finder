9ddf59de2c8e1c34ff90a7b94ef1f36454d3e733
Alfer Jay Salvador <alferjaysalvador@gmail.com>
2020-05-04 15:02:09 +0400

    Updates

---
+++ precache-manifest.3c344fe66ef196e7928f1a16aa5282ac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cb419a37d49447faef24a95d4bc873d",
+    "url": "/pixabay-image-finder/index.html"
+  },
+  {
+    "revision": "63fbea7aa0d55fd50508",
+    "url": "/pixabay-image-finder/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "7381345d48e58f06df4c",
+    "url": "/pixabay-image-finder/static/js/2.5ab98d0c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/pixabay-image-finder/static/js/2.5ab98d0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b6c7ad65b57980b6575",
+    "url": "/pixabay-image-finder/static/js/3.ad684775.chunk.js"
+  },
+  {
+    "revision": "5b6e1051d3f833affbf1",
+    "url": "/pixabay-image-finder/static/js/4.25c0c0ea.chunk.js"
+  },
+  {
+    "revision": "63fbea7aa0d55fd50508",
+    "url": "/pixabay-image-finder/static/js/main.2f909a7a.chunk.js"
+  },
+  {
+    "revision": "7e8644c5e706a7e6d9eb",
+    "url": "/pixabay-image-finder/static/js/runtime-main.b90fd708.js"
+  }
+]);
